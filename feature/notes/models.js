d8098cd877159a7996dab527278e13aaa3d63dbe
Julius Pedersen <deifyed@gmail.com>
2020-04-28 19:54:59 +0200

    ✅ Add support for last_updated

--- feature/notes/models.js
+++ feature/notes/models.js
@@ -2,11 +2,13 @@ class Note {
 	constructor(obj) {
 		this.id = obj.id || null
 		this.body = obj.body || ''
+		this.last_updated = obj.last_updated || ''
 	}
 
 	isValid() {
 		if (typeof this.id !== 'string') return false
 		if (typeof this.body !== 'string') return false
+		if (typeof this.last_updated !== 'string') return false
 
 		return true
 	}
